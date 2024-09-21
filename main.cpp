2fbeaaa978602090ed19d82e204e8b2c941881f2
Ikker Tibor Márton <ikkert14@gmail.com>
2024-09-21 14:16:57 +0200

    Hibák kommentezése

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS];  // N_ELEMENTS szükésges
+    std::cout << '1-100 ertekek duplazasa' // hiányos lezzárás
+    for (int i = 0;)  //hibás for ciklus
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:"  // nincs befejezve
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++)   // ; kell
     {
-        atlag += b[i]
+        atlag += b[i]   // hiányos ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
